bb5ce295d06e217130061370f18faa8ad41a37d3
Aysha Rahbar <aysharahbar06@gmail.com>
2025-05-29 17:57:31 +0530

    route file

---
+++ routes/routes.go
@@ -0,0 +1,23 @@
+package routes
+
+import (
+	"go-blog/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(postHandler *handlers.PostHandler) *gin.Engine {
+	router := gin.Default()
+	api := router.Group("/api")
+	{
+		api.GET("/posts", postHandler.ListPosts)
+		api.POST("/posts", postHandler.CreatePost)
+		api.POST("/posts/:id/reaction", postHandler.AddReaction)
+		api.DELETE("/posts/:id", postHandler.DeletePost)
+	}
+	router.GET("/posts", postHandler.ListPosts)
+	router.POST("/posts", postHandler.CreatePost)
+	router.DELETE("/posts/:id", postHandler.DeletePost)
+
+	return router
+}
