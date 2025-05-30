e813193c2ba57e8c902a087b90e4d9e225e3bdba
Aysha Rahbar <aysharahbar06@gmail.com>
2025-05-30 18:25:55 +0530

    error handling for service layer and couple of other files

--- service/post_service.go
+++ service/post_service.go
@@ -1,15 +1,17 @@
 package service
 
 import (
+	"errors"
 	"go-blog/models"
 	"go-blog/repo"
+	"strings"
 )
 
 type PostService interface {
-	GetAllPosts() []models.Post
-	CreatePost(post *models.Post) *models.Post
-	UpdatePost(id string, post *models.Post) *models.Post
-	DeletePost(id string)
+	GetAllPosts() ([]models.Post, error)
+	CreatePost(post *models.Post) (*models.Post, error)
+	UpdatePost(id string, post *models.Post) (*models.Post, error)
+	DeletePost(id string) error
 }
 
 type postService struct {
@@ -20,19 +22,32 @@ func NewPostService(repo repo.PostRepository) PostService {
 	return &postService{repo: repo}
 }
 
-func (s *postService) GetAllPosts() []models.Post {
+func (s *postService) GetAllPosts() ([]models.Post, error) {
 	return s.repo.ListPosts()
 }
 
-func (s *postService) CreatePost(post *models.Post) *models.Post {
+func (s *postService) CreatePost(post *models.Post) (*models.Post, error) {
+	if strings.TrimSpace(post.Title) == "" {
+		return nil, errors.New("title cannot be empty")
+	}
+	if strings.TrimSpace(post.Content) == "" {
+		return nil, errors.New("content cannot be empty")
+	}
+
 	return s.repo.CreatePost(post)
 }
 
-func (s *postService) UpdatePost(id string, post *models.Post) *models.Post {
-	updatedPost, _ := s.repo.Update(id, post)
-	return updatedPost
+func (s *postService) UpdatePost(id string, post *models.Post) (*models.Post, error) {
+	if strings.TrimSpace(post.Title) == "" {
+		return nil, errors.New("title cannot be empty")
+	}
+	if strings.TrimSpace(post.Content) == "" {
+		return nil, errors.New("content cannot be empty")
+	}
+
+	return s.repo.Update(id, post)
 }
 
-func (s *postService) DeletePost(id string) {
-	s.repo.DeletePost(id)
+func (s *postService) DeletePost(id string) error {
+	return s.repo.DeletePost(id)
 }
